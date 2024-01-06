4633bff6886267a9635e1faa80e7e929f149a6e1
Prince kumar <112043341+cutexboy@users.noreply.github.com>
2024-01-07 05:14:49 +0530

    Baby must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -24,7 +24,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("✨ ᴜᴘᴅᴀᴛᴇ ✨", url=link),
+                                InlineKeyboardButton("✨ ᴜᴘᴅᴀᴛᴇ ✨", url= https://t.me/FRIENDS2FAMILY_0),
                             ]
                         ]
                     )
